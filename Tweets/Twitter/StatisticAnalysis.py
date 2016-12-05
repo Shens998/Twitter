41e0f74d9e95d0c0531a34a653595f8fcfea81d1
shens <shenshi998@gmail.com>
2016-12-05 15:00:50 +0800

    statisc analysis

---
+++ Tweets/Twitter/StatisticAnalysis.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2016/12/5 11:18
+# @Author  : Shen Shi
+# @Site    : 
+# @File    : StatisticAnalysis.py
+# @Software: PyCharm Community Edition
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+links = np.genfromtxt(".\data\haiyan-links.csv", dtype=str, delimiter=',', skip_header=1, usecols=(0,1))
+dic = {}
+for n in sorted(np.reshape(links, 558)):
+    if n not in dic:
+        dic[n] = 1
+    else:
+        dic[n] += 1
+plt.bar(range(95), list(dic.values()))
+plt.xticks(range(95), list(dic.keys(), rotation=90))
+plt.show()
